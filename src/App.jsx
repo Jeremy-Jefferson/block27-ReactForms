38be46ad0c13b5271aa57ff8edb37ec481c6ee3d
jeremy-jefferson <jeremyjefferson34@gmail.com>
2023-08-07 10:25:56 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Authenticate from "./authenticate";
-import SignUpForm from "./signupform";
+import { useState } from "react";
+import "./App.css";
+import SignUpForm from "./Components/SignUpForm";
+import Authenticate from "./Components/Authenticate";
 
-export default function App() {
+function App() {
+  const [token, setToken] = useState(null);
+
   return (
     <>
-      <Authenticate />
-      <SignUpForm />
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
     </>
   );
 }
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
